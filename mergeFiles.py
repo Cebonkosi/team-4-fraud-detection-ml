23f189341eb94209d330383f8fe3ce4f86880bc8
Azola <105164748+Shojiki-Lukhozi@users.noreply.github.com>
2024-08-15 15:59:26 +0200

    ✨ Finalised Merge File Code

--- mergeFiles.py
+++ mergeFiles.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # Load the CSV files into DataFrames
-df1 = pd.read_csv('file1.csv')
-df2 = pd.read_csv('file2.csv')
+df1 = pd.read_csv('claims_preprocessed.csv')
+df2 = pd.read_csv('customers_preprocessed.csv')
 
 # Perform the merge on the PolicyId column
-merged_df = pd.merge(df1, df2, on='PolicyId')
+merged_df = pd.merge(df1, df2, on='policy_id')
 
 # Save the result to a new CSV file
-merged_df.to_csv('merged_file.csv', index=False)
+merged_df.to_csv('merged_customer_data.csv', index=False)
 
 print("CSV files have been joined and saved as merged_file.csv")
